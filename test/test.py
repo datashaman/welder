2a100b55046071ce6a656195b1e8d775a165d3ad
Marlin Forbes <marlinf@datashaman.com>
2011-03-28 08:41:31 +0200

    Up to Step 9 now passes

--- test/test.py
+++ test/test.py
@@ -1,8 +1,18 @@
 import os
-from lxml import etree
+from lxml import etree, cssselect
 from pyquery import PyQuery
 from weld import weld
-from nose.util import *
+from nose.tools import *
+
+data = (
+    dict(name='hij1nx', title='code exploder'),
+    dict(name='tmpvar', title='code pimp'),
+)
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 def get_template(name):
     filename = os.path.join(os.path.dirname(__file__), 'test.html')
@@ -13,23 +23,148 @@ def get_template(name):
             .clone()\
             .find('#' + name)
 
+def check_contacts(template, reversed=False):
+    eq_(template('.contact').length, 2)
+
+    if reversed:
+        indices = '21'
+    else:
+        indices = '12'
+
+    eq_(template('.contact:nth-child(%s) .foo' % indices[0]).text(), 'hij1nx')
+    eq_(template('.contact:nth-child(%s) .foo' % indices[1]).text(), 'tmpvar')
+
+    eq_(template('.contact:nth-child(%s) .title' % indices[0]).text(), 'code exploder')
+    eq_(template('.contact:nth-child(%s) .title' % indices[1]).text(), 'code pimp')
+
 def test_sanity():
     """Test 1: Sanity"""
-    template = PyQuery('<div><a class="link"></a></div>')('div')
+    template = PyQuery('<div><a class="link"></a></div>')
 
     def set_text(p, e, k, v):
         template(e).text('woo')
 
-    weld(template, dict(link='text'), dict(set=set_text, debug=True))
+    weld(template[0], dict(link='text'), dict(set=set_text))
     eq_(template.text(), 'woo')
 
 def test_object_literal():
     "Test 2: Assign data to elements using an object literal that has one level of depth"
     template = get_template('singular')
 
-    data = dict(key='someKey', value='someValue', icon='/path/to/image.png')
-    weld(template, data)
+    data = AttrDict(key='someKey', value='someValue', icon='/path/to/image.png')
+    weld(template[0], data)
 
-    eq_(template('.key').text(), data['key'])
-    eq_(template('.icon').attr('src'), data['icon'])
-    eq_(template('.input[name="value"]').val(), data['value'])
+    eq_(template('.key').text(), data.key)
+    eq_(template('.icon').attr('src'), data.icon)
+    eq_(template(':input[@name="value"]').val(), data.value)
+
+def test_alias():
+    """Test 3: Generate markup based on an element using the alias parameter to explicitly correlate data-keys and elements"""
+    template = get_template('contacts-alias')
+
+    weld(template('.contact')[0], data, dict(
+        alias=dict(name='foo', title='title')
+    ))
+
+    check_contacts(template)
+
+def test_alias_function():
+    """Test 4: Generate markup based on an element using an (alias w/function) parameter to explicitly correlate data and elements"""
+    template = get_template('contacts-alias')
+
+    def alias_name(p, e, k, v):
+        eq_(k, 'name')
+        return 'foo'
+
+    weld(template('.contact')[0], data, dict(alias=dict(name=alias_name,\
+        title='title')))
+
+    check_contacts(template)
+
+times = 0
+
+def test_alternate_insert():
+    """Test 5: Generate markup from an element with an alternate insert method"""
+    template = get_template('contacts')
+
+    def alternate_insert(p, e, k=None, v=None):
+        global times
+        times += 1
+        p.insert(0, e)
+
+    weld(template('.contact')[0], data, dict(insert=alternate_insert))
+
+    check_contacts(template, True)
+    eq_(times, 2)
+
+def test_reweld():
+    """Test 6: Append to a node that has already been the subject of a weld"""
+    template = get_template('contacts')
+
+    data = (
+        dict(name='hij1nx', title='manhattan'),
+        dict(name='tmpvar', title='brooklyn'),
+    )
+
+    contact_template = template('.contact')[0]
+    weld(contact_template, data)
+    weld(contact_template, data)
+
+    eq_(template('.contact:nth-child(1) .name').text(), 'hij1nx')
+    eq_(template('.contact:nth-child(2) .name').text(), 'tmpvar')
+    eq_(template('.contact:nth-child(3) .name').text(), 'hij1nx')
+    eq_(template('.contact:nth-child(4) .name').text(), 'tmpvar')
+
+    eq_(template('.contact:nth-child(1) .title').text(), 'manhattan')
+    eq_(template('.contact:nth-child(2) .title').text(), 'brooklyn')
+    eq_(template('.contact:nth-child(3) .title').text(), 'manhattan')
+    eq_(template('.contact:nth-child(4) .title').text(), 'brooklyn')
+
+    eq_(template('.contact').length, 4)
+    eq_(template('.contact .name').length, 4)
+    eq_(template('.contact .title').length, 4)
+
+def test_array():
+    """Test 7: Create markup from an array of objects that have one dimension"""
+    template = get_template('contacts')
+
+    weld(template('.contact')[0], data)
+
+    check_contacts(template)
+
+def test_unmatched_selectors():
+    """Test 8: Try to pair data with selectors that yield no matching elements"""
+    template = get_template('contacts-none')
+
+    data = (
+        dict(x01h='hij1nx', x0x1h='code exploder'),
+        dict(name='tmpvar', x0x1h='code wrangler'),
+    )
+
+    weld(template('.contact')[0], data)
+
+    ok_('tmpvar' in template('.name:eq(1)').text())
+    ok_('Leet Developer' in template('.title:eq(1)').text())
+
+def test_array_of_arrays():
+    """Test 9: Create markup from an object literal that has one dimension that contains an array of objects with one dimension"""
+    template = get_template('array-of-arrays')
+
+    data = AttrDict(person=(
+        AttrDict(name='John', job=('guru', 'monkey', 'tester')),
+        AttrDict(name='Bob', job=('supervise', 'yell')),
+    ), bar='hello')
+
+    def alternative_map(p, e, k, v):
+        template(e).addClass('pre-processed')
+
+    weld(template('.people')[0], data, dict(map=alternative_map))
+
+    eq_(template('.person:nth-child(1) .name').text(), 'John')
+    eq_(template('.person:nth-child(1) .job').length, 3)
+
+    eq_(template('.person:nth-child(2) .name').text(), 'Bob')
+    eq_(template('.person:nth-child(2) .job').length, 2)
+
+    eq_(template('.person.bar').text(), 'hello')
+    eq_(template('.person.bar').length, 1)
--- weld/__init__.py
+++ weld/__init__.py
@@ -2,15 +2,24 @@
 
 import inspect
 from lxml import etree
+from copy import deepcopy
 import collections, logging.config
 from pyquery import PyQuery
 
 logging.config.fileConfig('logging.ini')
 log = logging.getLogger(__name__)
 
+def d_label(action, element):
+    return '%s - element: %s, class: %s, id: %s' %\
+        (action.upper(), colorize(etree.tostring(element)),\
+                colorize(element.get('class')), colorize(element.get('id')))
+
+def d(action, element):
+    log.debug('- %s' % d_label(action, element))
+
 class AttrDict(dict):
-    def __init__(self, d = {}):
-        dict.__init__(self, d)
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
 depth = 0
@@ -22,23 +31,13 @@ color = AttrDict(dict(gray='\033[37m', darkgray='\033[40;30m', red='\033[31m',\
 successIndicator = color.green + ' ✓' + color.gray
 failureIndicator = color.red + ' ✗' + color.gray
 
-def pad():
-    global depth
-
-    l = depth
-    ret = ''
-
-    while(l > 0):
-        ret += '│    '
-        l = l - 1
-
-    return ret
+pad = lambda: '│    ' * depth
 
 def colorize(val):
     sval = str(val)
 
-    if sval is 'False' or sval is 'None' or sval is "" or val is False:
-        if sval is "":
+    if sval in ('False', 'None', '') or val is False:
+        if sval is '':
             sval = '(empty string)'
         return color.red + sval + color.gray
     else:
@@ -46,144 +45,205 @@ def colorize(val):
 
     return sval
 
-
 def debuggable(name, func=None):
     label = name.upper()
 
-    def new_func(p, e, k, v):
+    def new_func(parent, element, key=None, value=None):
         global depth
+        depth += 1
 
         log.debug('%s%s┌ %s - parent:%s, element:%s, key:%s, value:%s'\
-                % (pad(), color.gray, label, colorize(p), colorize(e),\
-                    colorize(k), colorize(v)))
-        depth = depth + 1
+                % (pad(), color.gray, label,\
+                    colorize(parent is not None and etree.tostring(parent) or 'None'),\
+                    colorize(etree.tostring(element)),\
+                    colorize(key), colorize(value)))
 
-        if func:
-            res = func(p, e, k, v)
-            depth = depth - 1
+        if inspect.isfunction(func):
+            res = func(parent, element, key, value)
+            depth -= 1
             if res is not False:
                 indicator = successIndicator
             else:
                 indicator = failureIndicator
 
-            log.debug('%s└ %s%s' % (pad(), e.__html__(), indicator))
+            log.debug('%s└ %s%s' % (pad(), etree.tostring(element), indicator))
             return res
 
-        depth = depth - 1
-        d('- OPERATION NOT FOUND: ', label)
+        depth -= 1
+        d('OPERATION NOT FOUND', label)
 
     return new_func
 
 def weld(DOMTarget, data, pconfig={}):
-    def element_type(p, e, k, v):
-        if e:
-            node_name = e[0].tag
+    assert isinstance(DOMTarget, etree._Element)
 
-            if isinstance(node_name, str):
-                if node_name.lower() in ('input', 'select', 'textarea',\
-                        'option', 'button'):
-                    return 'input'
+    def _check_args(parent, element):
+        assert parent is None or isinstance(parent, etree._Element)
+        assert isinstance(element, etree._Element)
 
-                if node_name == 'img':
-                    return 'image'
+    def siblings(parent, element, key, value):
+        _check_args(parent, element)
 
-    def map(p, e, k, v):
-        return True
+        siblings = parent.getchildren()
+        cs = len(siblings)
 
-    def set(p, e, k, v):
-        if ops.map(p, e, k, v) == False:
-            return False
+        element.weld = AttrDict(parent=parent,\
+                classes=element.get('class', '').split(' '), insertBefore=None)
 
-        if debug:
-            log.debug('- SET: value is %s' % v.tagName)
+        while cs:
+            cs -= 1
+            sibling = siblings[cs]
 
-        type = ops.element_type(p, e, k, v)
-        res = False
+            if sibling is element:
+                if debug:
+                    d('remove', element)
 
-        if None not in (v, v.nodeType):
-            if e.ownerDocument != v.ownerDocument:
-                value = e.ownerDocument.importNode(v, true)
-            elif v.getparent() is not None:
-                v.getparent().removeChild(v)
+                index = parent.index(element)
+                parent.remove(element)
 
-            while e.firstChild is not None:
-                e.removeChild(e.firstChild)
+                if index < len(parent):
+                    element.weld.insertBefore = parent[index]
+            else:
+                classes = sibling.get('class', '').split(' ')
+                match = True
 
-            e.appendChild(v)
-            res = True
-        elif type == 'input':
-            e.setAttribute('value', v)
-            res = True
-        elif type == 'image':
-            e.setAttribute('src', v)
-            res = True
-        else:
-            e.textContent = v
-            res = True
+                for _class in classes:
+                    if _class not in element.weld.classes:
+                        match = False
+                        break
 
-        return res
+                if match:
+                    if debug:
+                        d('remove', sibling)
 
-    def match(p, e, k, v):
-        if 'alias' in config:
-            if config.alias and k in config.alias:
-                if inspect.isfunction(config.alias[k]):
-                    k = config.alias[k](p, e, k, v) or k
-                elif config.alias[k] is False:
-                    return False
-                else:
-                    k = config.alias[k]
+                    parent.remove(sibling)
 
-        if k and hasattr(k, 'nodeType'):
-            return k
+    def traverse(parent, element, key, value):
+        _check_args(parent, element)
 
-        if e:
-            selector = '.{0}, #{0}, [name="{0}"]'.format(k)
-            return e(selector)
+        template = element
+        templateParent = element.getparent()
 
-    def traverse(p, e, k, v):
-        template = e
-        templateParent = e.parent()
-
-        if v is None or isinstance(v, str):
-            ops.set(p, e, k, v)
-        elif isinstance(v, collections.Sequence):
+        if value is None or isinstance(value, str) or isinstance(value, etree._Element):
+            ops.set(parent, element, key, value)
+        elif isinstance(value, collections.Sequence) and len(value) and value[0] is not None:
             if templateParent is not None:
-                ops.siblings(templateParent, template, k, v)
+                ops.siblings(templateParent, template, key, value)
             elif None not in (template.weld, template.weld.parent):
                 templateParent = template.weld.parent
 
-            for i, val in enumerate(value):
+            for index, obj in enumerate(value):
                 if debug:
-                    log.debug('- CLONE - element: %s class: %s id: %s' % (element, element.className, element.id))
+                    d('clone', element)
 
-                target = e.clone()
-                target.weld = {}
+                target = deepcopy(element)
+                target.weld = AttrDict()
 
                 if element.weld is not None:
                     target.weld.update(element.weld)
 
-                ops.traverse(templateParent, target, i, val)
+                ops.traverse(templateParent, target, index, obj)
                 ops.insert(templateParent, target)
         else:
-            for key, obj in v.items():
-                target = ops.match(template, e, key, obj)
-                if target:
+            for key, obj in value.items():
+                target = ops.match(template, element, key, obj)
+                if target is not None:
                     ops.traverse(template, target, key, obj)
 
-    parent = DOMTarget.parent()
+    def insert(parent, element, key=None, value=None):
+        _check_args(parent, element)
+
+        if None not in (element.weld, element.weld.insertBefore):
+            if debug:
+                log.debug('Insert %s before element %s in %s' %
+                    (etree.tostring(element),
+                        etree.tostring(element.weld.insertBefore),
+                        etree.tostring(parent)))
+            parent.insert(parent.index(element.weld.insertBefore), element)
+        else:
+            parent.append(element)
+
+    def element_type(parent, element, key, value):
+        _check_args(parent, element)
+
+        if isinstance(element, etree._Element):
+            node_name = element.tag
+
+            if node_name.lower() in ('input', 'select', 'textarea',\
+                    'option', 'button'):
+                return 'input'
+
+            if node_name == 'img':
+                return 'image'
+
+    def map(parent, element, key, value):
+        _check_args(parent, element)
+
+        return True
+
+    def set(parent, element, key, value):
+        _check_args(parent, element)
+
+        if ops.map(parent, element, key, value) is False:
+            return False
+
+        if debug:
+            log.debug('- SET: element:%s, key:%s, value:%s' % (element.tag, key, value))
+
+        type = ops.element_type(parent, element, key, value)
+
+        if value is not None and isinstance(value, etree._Element):
+            if element.ownerDocument != value.ownerDocument:
+                value = element.ownerDocument.importNode(value, true)
+            elif value.getparent() is not None:
+                value.getparent().removeChild(value)
+
+            while element.firstChild is not None:
+                element.removeChild(element.firstChild)
+
+            element.appendChild(value)
+        elif type == 'input':
+            element.set('value', value)
+        elif type == 'image':
+            element.set('src', value)
+        else:
+            element.text = value
+
+        return True
+
+    def match(parent, element, key, value):
+        _check_args(parent, element)
+
+        if 'alias' in config:
+            if config.alias and key in config.alias:
+                if inspect.isfunction(config.alias[key]):
+                    key = config.alias[key](parent, element, key, value) or key
+                elif config.alias[key] is False:
+                    return False
+                else:
+                    key = config.alias[key]
+
+        if isinstance(key, etree._Element):
+            return key
+
+        if element is not None:
+            selector = "//*[contains(@class, '{0}')] | //*[@id='{0}'] | //*[@name='{0}']".format(key)
+            result = element.xpath(selector)
+            if result:
+                return result[0]
+
+    parent = DOMTarget.getparent()
 
     config = AttrDict(dict(alias={}, debug=False, insert=False))
     config.update(pconfig)
 
     debug = config['debug']
 
-    print debug
-
-    ops = AttrDict(dict(filter(lambda i: inspect.isfunction(i[1]),\
+    ops = AttrDict(dict(filter(lambda index: inspect.isfunction(index[1]),\
         locals().items())))
 
     for name, func in ops.items():
-        if name in config:
+        if name in config and config[name]:
             func = config[name]
 
         if debug:
@@ -201,7 +261,7 @@ def weld(DOMTarget, data, pconfig={}):
 
         log.debug(debug)
 
-def pyquery_weld(data, config=dict(debug=True)):
+def pyquery_weld(data, config={}):
     weld(this[0], data, config)
     return this
 
