952f0a9594324074666c9446566b481fa2c68ab3
Marlin Forbes <marlinf@datashaman.com>
2011-03-30 18:15:34 +0200

    Changes for publication

--- setup.py
+++ setup.py
@@ -1,30 +1,41 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='welder',
-      version=version,
-      description="",
-      long_description="""\
+  version=version,
+  description="HTML/XML server-side transformer. Port of hij1nk's weld for node.js.",
+  long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          'lxml',
-          'pyquery',
-          'nose',
-          'coverage'
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+    'Topic :: Text Processing :: Markup :: HTML',
+    'Topic :: Text Processing :: Markup :: XML',
+  ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+  keywords='',
+  author='Marlin Forbes',
+  author_email='marlinf@datashaman.com',
+  url='https://github.com/datashaman/welder',
+  license='Open Source Initiative OSI - The MIT License',
+  packages=find_packages(exclude=['test']),
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=[
+      # -*- Extra requirements: -*-
+      'lxml',
+      'pyquery',
+      'nose',
+      'coverage'
+  ],
+)
